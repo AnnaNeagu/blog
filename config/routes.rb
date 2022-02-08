360cafdd64a5b2ef7bcc3fbe885e4217ac5e57ca
Ana Neagu <Ana.Neagu@theaccessgroup.com>
2022-02-08 08:48:13 +0200

    update authentication + no errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  devise_for :users, controller: {
+    sessions: 'users/session'
+  }
+
   root "articles#index"
 
   resources :articles do
